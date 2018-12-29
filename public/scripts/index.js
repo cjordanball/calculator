dbd3b8b03ec6c295490d55fcb83b703ff8e66300
Jordan Ball <jordan@cjordanball.info>
2018-12-28 23:59:23 -0500

    save button work

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,10 @@
+import NumberKey from './components/numberKey.js';
 const container = document.createElement('div');
 container.classList.add('container');
 const body = document.querySelector('body');
 body.appendChild(container);
+
+for (let i = 0 ; i < 10; i++) {
+	const numKey = NumberKey(i);
+	container.appendChild(numKey);
+}

78fcf8784d09a7980a521ed092d2941170c088e4
Jordan Ball <jordan@cjordanball.info>
2018-12-30 15:43:08 -0500

    add rows to hold number keys

--- public/scripts/components/keys/numberContainer.js
+++ public/scripts/components/keys/numberContainer.js
@@ -16,12 +16,40 @@ const handleInput = (data) => {
 
 const NumberContainer = () => {
 	const numberContainer = document.createElement('div');
+	const numberRow1 = document.createElement('div');
+	numberRow1.classList.add('numberRow');
+
+	const numberRow2 = document.createElement('div');
+	numberRow2.classList.add('numberRow');
+
+	const numberRow3 = document.createElement('div');
+	numberRow3.classList.add('numberRow');
+
+	const numberRow4 = document.createElement('div');
+	numberRow4.classList.add('numberRow');
+
 	numberContainer.classList.add('numberBox');
 
 	for (let i = 0 ; i < 10; i++) {
 		const numKey = NumberKey(i);
-		numberContainer.appendChild(numKey);
+		if (i === 0) {
+			numberRow4.appendChild(numKey);
+			continue;
+		} else if (i < 4) {
+			numberRow1.appendChild(numKey);
+			continue;
+		} else if (i < 7) {
+			numberRow2.appendChild(numKey);
+			continue;
+		} else {
+			numberRow3.appendChild(numKey);
+			continue;
+		}
 	}
+	numberContainer.appendChild(numberRow1);
+	numberContainer.appendChild(numberRow2);
+	numberContainer.appendChild(numberRow3);
+	numberContainer.appendChild(numberRow4);
 	numberContainer.addEventListener('click', (e) => {handleInput(e.target.dataset.number)});
 	return numberContainer;
 };
